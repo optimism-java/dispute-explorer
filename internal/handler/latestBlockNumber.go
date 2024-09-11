509f7d9b014dbeaebfaf870b1782ecc38f8b7ac0
zhouop0 <11733741+zhouop0@users.noreply.github.com>
2024-09-11 16:13:47 +0800

    format

--- internal/handler/latestBlockNumber.go
+++ internal/handler/latestBlockNumber.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"time"
 
-	"github.com/pkg/errors"
-
 	"github.com/optimism-java/dispute-explorer/internal/svc"
 	"github.com/optimism-java/dispute-explorer/pkg/log"
+	"github.com/pkg/errors"
+	"github.com/spf13/cast"
 )
 
 func LatestBlackNumber(ctx *svc.ServiceContext) {
@@ -19,7 +19,7 @@ func LatestBlackNumber(ctx *svc.ServiceContext) {
 			continue
 		}
 
-		ctx.LatestBlockNumber = int64(latest)
+		ctx.LatestBlockNumber = cast.ToInt64(latest)
 		log.Infof("[Handle.LatestBlackNumber] Syncing latest block number: %d \n", latest)
 		time.Sleep(3 * time.Second)
 	}
