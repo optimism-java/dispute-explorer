7c4321b94b117228f6b279fdfbab0dc59aa17e9e
zhouop0 <11733741+zhouop0@users.noreply.github.com>
2024-09-13 15:32:37 +0800

    fix clock type

--- migration/version/migration_version.go
+++ migration/version/migration_version.go
@@ -7,6 +7,7 @@ import (
 	v3 "github.com/optimism-java/dispute-explorer/migration/version/v3"
 	v4 "github.com/optimism-java/dispute-explorer/migration/version/v4"
 	v5 "github.com/optimism-java/dispute-explorer/migration/version/v5"
+	v6 "github.com/optimism-java/dispute-explorer/migration/version/v6"
 )
 
 var ModelSchemaList = []*gormigrate.Migration{
@@ -15,4 +16,5 @@ var ModelSchemaList = []*gormigrate.Migration{
 	&v3.UpdateLostBondAndClaimDataTable,
 	&v4.UpdateClaimDataPositionColumnTable,
 	&v5.AddOnChainStatusForDisputeGameTable,
+	&v6.UpdateClaimDataClockColumnTable,
 }
